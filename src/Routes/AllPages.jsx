a47ad5990cc43714b959f7dc80cc3db1161ecdc3
Pedro Carvalho Almeida <80465335+pedrocarvalhosnk@users.noreply.github.com>
2022-11-28 19:39:06 -0400

    Estrutura de paginas e rotas

---
+++ src/Routes/AllPages.jsx
@@ -0,0 +1,21 @@
+import { NavigationContainer, StackActions } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Start from "../Pages/Start";
+
+const Stack = createNativeStackNavigator();
+
+export default function AllPages(){
+  return(
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShow:false,
+        }}
+      >
+      <Stack.Screen name="Start" componet={Start}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+
+  )
+}
